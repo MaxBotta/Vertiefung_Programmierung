45f6f84a67e6a70310b99a6a0de26a5b49dd5fa6
MaxBotta <max.botta@gmx.de>
2017-11-05 11:22:19 +0100

    email validierung, Input Abfrage

--- Max/Adressbuch/Adressbuch.py
+++ Max/Adressbuch/Adressbuch.py
@@ -1,7 +1,7 @@
 import crud
 
 contacts = crud.read()
-range = {"anrede": 10, "name": 15, "vorname": 15, "strasse": 20, "hausnummer:": 15, "plz": 10, "stadt": 15, "telefon1": 15, "telefon2": 15, "email": 20}
+range = {"Anrede": 10, "Name": 15, "Vorname": 15, "Straße": 20, "Hausnummer": 15, "PLZ": 10, "Stadt": 15, "Telefon1": 15, "Telefon2": 15, "Email": 20}
 
 
 def set_spacing(string, x):
@@ -30,6 +30,104 @@ def is_in_range(value, header):
     if len(value) <= range[header]:
         return True
     return False
+
+def is_number_or_alpha(value):
+    if is_number(value) or is_alphabetic(value):
+        return True
+    return False
+
+
+#print(is_alphabetic("Herr") and is_in_range("Herr", "Anrede"))
+
+
+def is_email(value):
+    at = False
+    second_at = False
+    before = False
+    after = False
+    dot = False
+    space = False
+
+    # Überprüfe ob ein @ existiert und ob es mehr als ein @ gibt
+    at_index = value.find("@")
+    second = value.find("@", at_index + 1)
+
+    if at_index >= 0:
+        at = True
+    if second >= 0:
+        second_at = True
+
+    # Überprüfe ob Zeichen vor und nach dem @-Zeichen existieren
+    if is_number_or_alpha(value[at_index-1]):
+        before = True
+    if is_number_or_alpha(value[at_index+1]):
+        after = True
+
+    # Überprüfe ob mindestens ein Punkt vorhanden ist
+    if value.find(".") >= 0:
+        dot = True
+
+    # Überprüfe ob ein Leerzeichen existiert
+    if value.find(" ") >= 0:
+        space = True
+
+    if at and not second_at and before and after and dot and not space:
+        return True, "Email validiert!"
+    elif not at:
+        return False, "@-Zeichen vergessen!"
+    elif second_at:
+        return False, "Zu viele @-Zeichen!"
+    elif not before or not after:
+        return False, "Vor und nach dem @ muss ein Zeichen stehen!"
+    elif not dot:
+        return False, "Punkt vergessen!"
+    elif space:
+        return False, "Leerzeichen vorhanden!"
+
+
+
+
+
+
+test = "maxgmail.com"
+print(is_email(test)[1])
+
+# Alle Abfragen laufen über diese Funktion. Ermöglicht sich wiederholende Abfragen,
+# sowie die Abfrage einzelner Eingaben.
+def get_input(header):
+    repeat = True
+    while repeat:
+        answer = input("--> " + header + ": (Max. " + str(range[header]) + " Zeichen)")
+
+        # Wenn es sich um eine numerische Eingabe handelt
+        if header == "Hausnummer" or header == "PLZ" or header == "Telefon1" or header == "Telefon2":
+            # Auf Ziffern prüfen und ob es zu viele Zeichen sind
+            if is_number(answer) and is_in_range(answer, header):
+                repeat = False
+                return answer
+            else:
+                print("Falsche Eingabe oder zu viele Zeichen!")
+
+        # Wenn es sich um die Email handelt. Hier sind alle Zeichen erlaubt
+        # und es wird auf ein @ Zeichen geprüft
+        elif header == "Email":
+
+            if is_email(answer) and is_in_range(answer, header):
+                repeat = False
+                return answer
+            else:
+                print("Falsche Eingabe (@ Zeichen vergessen?) oder zu viele Zeichen!")
+
+        # Wenn es sich um eine alphabetische Eingabe handelt
+        else:
+
+            if is_alphabetic(answer) and is_in_range(answer, header):
+
+                repeat = False
+                return answer
+            else:
+                print(answer)
+                print("Falsche Eingabe oder zu viele Zeichen!")
 
 
 def show_list():
@@ -60,42 +158,44 @@ def show_list():
 
 
 def add_item():
-    repeat = True
-    anrede = input("--> Anrede: (Max. 10 Zeichen)")
-    name = input("--> Nachname: (Max. 15 Zeichen)")
-    vorname = input("--> Vorname: (Max. 15 Zeichen)")
-    strasse = input("--> Straße: (Max. 20 Zeichen)")
-    hausnummer = input("--> Hausnummer: (Max. 15 Zeichen)")
-    plz = input("--> Postleitzahl: (Max. 10 Zeichen)")
-    stadt = input("--> Stadt: (Max. 15 Zeichen)")
-    telefon1 = input("--> Telefon 1: (Max. 15 Zeichen)")
-    telefon2 = input("--> Telefon 2: (Max. 15 Zeichen)")
-    email = input("--> Email: (Max. 20 Zeichen)")
+    anrede = get_input("Anrede")
+    name = get_input("Name")
+    vorname = get_input("Vorname")
+    strasse = get_input("Straße")
+    hausnummer = get_input("Hausnummer")
+    plz = get_input("PLZ")
+    stadt = get_input("Stadt")
+    telefon1 = get_input("Telefon1")
+    telefon2 = get_input("Telefon2")
+    email = get_input("Email")
 
-    if input_validation(new_item):
-        if len(new_item) <= 16:
-            amount = input("--> Wie viel von " + new_item + " möchten Sie hinzufügen?")
-            if amount_input_validation(amount):
-                answer = input("--> Möchten Sie " + new_item + " wirklich der Liste hinzufügen? (ja oder nein)")
-                while repeat:
-                    if answer == "ja":
-                        repeat = False
-                        new_entry(new_item, amount)
-                        print("Produkt %s wurde dem Einkaufszettel hinzugefügt" % new_item)
-                    elif answer == "nein":
-                        repeat = False
-                        print("Abgebrochen!")
-                    else:
-                        repeat = True
-                        print("Falsche Eingabe!")
-                        answer = input("--> Möchten Sie " + new_item + " wirklich der Liste hinzufügen? (ja oder nein)")
-            else:
-                print("Falsche Eingabe! (Nur Zahlen)")
-        else:
-            print("Zu viele Zeichen! (Maximal 16)")
+    # if input_validation(new_item):
+    #     if len(new_item) <= 16:
+    #         amount = input("--> Wie viel von " + new_item + " möchten Sie hinzufügen?")
+    #         if amount_input_validation(amount):
+    #             answer = input("--> Möchten Sie " + new_item + " wirklich der Liste hinzufügen? (ja oder nein)")
+    #             while repeat:
+    #                 if answer == "ja":
+    #                     repeat = False
+    #                     new_entry(new_item, amount)
+    #                     print("Produkt %s wurde dem Einkaufszettel hinzugefügt" % new_item)
+    #                 elif answer == "nein":
+    #                     repeat = False
+    #                     print("Abgebrochen!")
+    #                 else:
+    #                     repeat = True
+    #                     print("Falsche Eingabe!")
+    #                     answer = input("--> Möchten Sie " + new_item + " wirklich der Liste hinzufügen? (ja oder nein)")
+    #         else:
+    #             print("Falsche Eingabe! (Nur Zahlen)")
+    #     else:
+    #         print("Zu viele Zeichen! (Maximal 16)")
+    #
+    # else:
+    #     print("Falsche Eingabe! (Nur Buchstaben)")
 
-    else:
-        print("Falsche Eingabe! (Nur Buchstaben)")
+
+#add_item()
 
 
 def change_item():
