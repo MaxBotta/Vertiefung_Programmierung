ed66d141399494ffabeb4e8df8677101fd0a8f51
MaxBotta <max.botta@gmx.de>
2017-11-16 10:18:55 +0100

    Benotete Aufgabe 2.1 95% fertig - Fehlt: Erneute Abfrage oder Programm beenden

--- BenoteteAufgabe1/Teilaufgabe2.py
+++ BenoteteAufgabe1/Teilaufgabe2.py
@@ -1,5 +1,6 @@
 import BenoteteAufgabe1.CrudOperationen as Crud
 import csv
+import os
 
 
 def read(path, type):
@@ -45,7 +46,7 @@ def search_for_header_and_key(d, header, keyword):
     return result_list
 
 
-def get_keyword():
+def get_keyword_input():
     while True:
         print("Geben Sie ein Suchwort ein.")
         keyword = input("Ihre Eingabe: ")
@@ -57,7 +58,7 @@ def get_keyword():
 
 # Feldnamen abfragen.
 # Wiederholen, bis eine korrekte Eingabe vorgenommen wurde.
-def get_fieldname(d):
+def get_fieldname_input(d):
     while True:
         fieldname = input("Ihre Eingabe: ")
         if fieldname.isnumeric() and int(fieldname) <= len(d):
@@ -108,6 +109,7 @@ def get_range_list(d):
 print(get_range_list(daten))
 
 
+# TODO Try catch falls es ein Problem mit der Darstellung gibt, einfach Liste ausgeben.
 def show_result(d):
     print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
     print("Ergebnis")
@@ -129,17 +131,85 @@ def show_result(d):
     print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
 
+def check_if_file_exists(name):
+    file_list = get_all_csv()
+    print(file_list)
+    for item in file_list:
+        if item == name + ".csv":
+            return True
+    return False
+
+
+def save_as_csv(d):
+    while True:
+        print("Bitte geben Sie einen Dateinamen ein: ")
+        file_name = input("Ihre Eingabe: ")
+
+        # Überprüfen, ob eine Datei mit diesem Namen bereits existiert.
+        name_exists = check_if_file_exists(file_name)
+
+        # Falls diese existiert überschreiben oder erneut nach dem Namen fragen.
+        if name_exists:
+            print("Eine Datei mit diesem namen existiert bereits. Möchten Sie diese überschreiben? (j/n)")
+            overwrite = input("Ihre Eingabe: ")
+            while True:
+                if overwrite == "j":
+                    Crud.write(file_name + ".csv", d, get_fieldnames(d))
+                    print("Gespeichert!")
+                    return
+                elif overwrite == "n":
+                    break
+                else:
+                    print("Falsche Eingabe!")
+
+        elif not name_exists:
+            Crud.write(file_name + ".csv", d, get_fieldnames(d))
+            print("Gespeichert!")
+            return
+
+
+def get_fieldnames(d):
+    fieldnames = []
+    for key in d[0]:
+        fieldnames.append(key)
+    return fieldnames
+
+
+def get_all_csv():
+    # Liste mit sämtlichen Dateien ertsellen, die sich im selben Ordner befinden.
+    dir_list = os.listdir('.')
+    new_list = []
+    # Jeden Dateinamen mit der Endung '.csv' der Liste new_list hinzufügen.
+    for file in dir_list:
+        if file.find(".csv") > -1:
+            new_list.append(file)
+    return new_list
+
+
 def suchen_von_daten(d):
     # ---1. Suchfeld angeben und überprüfen.---
     print("In welchem Feld möchten Sie suchen?")
     show_header_with_index(d)
-    suchfeld = get_fieldname(d)
+    suchfeld = get_fieldname_input(d)
     # ---2. Suchwort angeben und überprüfen.---
-    suchwort = get_keyword()
+    suchwort = get_keyword_input()
     # ---3. Nach Einträgen suchen.---
     result_list = search_for_header_and_key(d, suchfeld, suchwort)
     # ---4. Ergebnis in der Konsole ausgeben oder als CSV Speichern.---
-    show_result(result_list)
+    if len(result_list) == 0:
+        print("Keine Einträge gefunden!")
+        return
+
+    while True:
+        csv_or_console = input("Möchten Sie die Ergebnisse als CSV-Datei ('csv') speichern oder in der Konsole ('con') anzeigen?")
+        if csv_or_console == "con":
+            show_result(result_list)
+            return
+        elif csv_or_console == "csv":
+            save_as_csv(result_list)
+            return
+        else:
+            print("Falsche Eingabe!")
 
 
 suchen_von_daten(daten)
