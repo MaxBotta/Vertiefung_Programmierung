e9f6c67a1c2e871dfb5428c7e2e7b9df8e70d082
Benutzer1 <tr064@hdm-stuttgart.de>
2017-12-28 16:12:25 +0100

    ergänzungen und korrekturen 3 inproceedings & proceedings

--- Tobi/Benotete_Abgabe3/Teilaufgabe1.py
+++ Tobi/Benotete_Abgabe3/Teilaufgabe1.py
@@ -2,9 +2,9 @@ from lxml import etree
 
 
 
-#datei = './test.xml'
+datei = './test.xml'
 # Die XML-Datenbasis
-datei = './dblp-2017-05-02.xml'
+#datei = './dblp-2017-05-02.xml'
 # die DTD-Datei der XML-Datenbasis, sie ermöglicht ein Validieren von XML-Elementen z.B. während des Parsens.
 dtd = etree.DTD('./dblp-2017-03-29.dtd')
 
@@ -42,7 +42,7 @@ def read_the_first_3_proceedings_and_inproceedings(datei):
     proceedings_tree = etree.ElementTree(proceedings_root)
 
     # iterparser zum Einlesen der Datei unter Berücksichtigung ausschließlich der "End"-Events
-    context = etree.iterparse(datei, events=('end', ), load_dtd=True, encoding='ISO-8859-1', huge_tree=True)
+    context = etree.iterparse(datei, events=('end', ), load_dtd=True, encoding='ISO-8859-1')
     # Iterieren über alle Elemente des Iterparser
     for event, elem in context:
         # die ersten drei inproceeding-elemente werden als subelemente des root elements hinzugefügt.
@@ -55,16 +55,20 @@ def read_the_first_3_proceedings_and_inproceedings(datei):
             proceedings_root.append(elem)
 
         elif counter_inproceedings >= 3 and counter_proceedings >= 3:
-            # sobald jeweils 3 elemente zu den jeweiligen ElementTrees hinzugefügt wurden, werden diese gegen die DTD validiert.
+            # sobald jeweils 3 Elemente zu den jeweiligen ElementTrees hinzugefügt wurden, werden diese gegen die DTD validiert.
             if dtd.validate(inproceedings_tree) and dtd.validate(proceedings_tree):
 
                 # Wenn die Validierungen "True" zurückgeben, werden die Elemente in die jeweiligen xml-Dateien geschrieben.
+                # Grundsätzlich sind durch den Parameter "load_dtd=True" beim Parsen schon alle Elemente validiert, hier werden jedoch zusätzlich ggf. vorgenommene Änderungen validiert.
                 with open('sample_inproceedings.xml', 'wb') as ausgabe1:
                     inproceedings_tree.write(ausgabe1, pretty_print=True, xml_declaration=True, encoding='ISO-8859-1', doctype="<!DOCTYPE dblp SYSTEM 'dblp-2017-03-29.dtd'>")
 
                 with open('sample_proceedings.xml', 'wb') as ausgabe2:
                     proceedings_tree.write(ausgabe2, pretty_print=True, xml_declaration=True, encoding='ISO-8859-1', doctype="<!DOCTYPE dblp SYSTEM 'dblp-2017-03-29.dtd'>")
-
+                return
+            else:
+                print("Validierung fehlgeschlagen!")
+                print(dtd.error_log.filter_from_errors())
                 return
 
 
