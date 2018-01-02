307563bc65e801d3020051ef784b00455f320f01
MaxBotta <max.botta@gmx.de>
2018-01-02 18:59:19 +0100

    Teilaufgabe 3 Nr. 2 fertig

--- Benotete_Abgabe3/Teilaufgabe3/Aufgabe1.py
+++ Benotete_Abgabe3/Teilaufgabe3/Aufgabe1.py
@@ -26,7 +26,7 @@ def get_inproceedings_by_editor(wanted_editor):
         # Alle Editors durchgehen und prüfen, ob dieser vorhanden ist
         try:
             editors = inproceeding["proc:editor"]
-            # Prüfen, ob es eine Liste, wenn ja Liste durchgehen
+            # Prüfen, ob es eine Liste ist, wenn ja Liste durchgehen
             if type(editors) == list:
                 for editor in editors:
                     if editor == wanted_editor:
@@ -44,5 +44,17 @@ def get_inproceedings_by_editor(wanted_editor):
     return list_of_inproceedings
 
 
-inproceedings_from_MichaelBrodie = get_inproceedings_by_editor("Piotr Dembinski")
-print(inproceedings_from_MichaelBrodie)
+def save_as_csv(filename, data):
+    try:
+        with open(filename + ".csv", "w", newline='', encoding="utf8") as file:
+            fieldnames = ['author', 'title', 'pages', 'proc:editor', 'proc:title']
+            writer = csv.DictWriter(file, fieldnames, extrasaction='ignore')
+            writer.writeheader()
+            writer.writerows(data)
+
+    except IOError:
+        print('An error occured trying to write the file.')
+
+
+inproceedings_from_MichaelBrodie = get_inproceedings_by_editor("Michael L. Brodie")
+save_as_csv("inproceedings_from_MichaelBrodie", inproceedings_from_MichaelBrodie)
